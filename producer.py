0b45662afae81ce1086ac642c7cf4918fbe183e2
Stefano Antonelli <stefano.antonelli.2024@gmail.com>
2025-01-09 20:00:21 +0100

    feat: added producer

--- producer.py
+++ producer.py
@@ -1,4 +1,27 @@
 import pika
 
+import time
+time.sleep(0.01)  # Ritardo di 10 ms tra i messaggi
+
 print('collegato a rabbit')
 
+params = pika.ConnectionParameters(host="localhost")
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+channel.queue_declare(queue='worker_queue')
+
+print('eseguito!')
+
+i = 0
+while True:
+    message = str(i)
+    i += 1
+
+    channel.basic_publish(exchange='', routing_key='worker_queue', body=message)
+    print('messaggio inviato', message)
+
+    if i > 1000:
+        break
+
+connection.close()    
